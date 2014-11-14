1468eddafa0410241356cf9327886d46e1ffb105
unknown <usingh@USINGH01-PC.hq.netapp.com>
2014-11-14 22:33:05 +0530

    deleting un-nessary files from server

--- svcProject/src/indwin/c3/lift/util/DistanceType.java
+++
@@ -1,6 +0,0 @@
-package indwin.c3.lift.util;
-
-public enum DistanceType {
-
-	BUS_TO_CURRENT, BUS_TO_DESTINATION, BUS_TO_DESTINATION_WAYPOINT;
-}
--- svcProject/src/indwin/c3/lift/util/ResponseStatus.java
+++
@@ -1,7 +0,0 @@
-package indwin.c3.lift.util;
-
-public class ResponseStatus {
-	
-	public static final int SUCCESS = 200;
-	public static final int FAILURE = 500;
-}
